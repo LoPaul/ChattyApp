4c7bbe90c6d126322dc41d9e8919af25f61c65e8
Paul <paul.c.lo@gmail.com>
2018-08-07 02:19:01 +0000

    app.jsc

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: true};
+  }
   render() {
     return (
       <h1>Hello React :)</h1>
